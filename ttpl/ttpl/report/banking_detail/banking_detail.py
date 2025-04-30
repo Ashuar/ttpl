9e596d9202dda7e06d7fedeccb1a071bb9f5146e
Ashuar <ashuarchughtai@gmail.com>
2025-04-30 12:59:28 +0500

    commiting changes

--- ttpl/ttpl/report/banking_detail/banking_detail.py
+++ ttpl/ttpl/report/banking_detail/banking_detail.py
@@ -46,48 +46,49 @@ def execute(filters=None):
         AND pe.posting_date >= '{filters.get("from_date")}'
         AND pe.posting_date <= '{filters.get("to_date")}'
 	"""
-    supplier_sql = f"""
-    SELECT
-		pe.party AS supplier,
-        pe.party_name,
-        {",".join(_banks)}
-	FROM
-		`tabPayment Entry` pe
-    LEFT JOIN
-		`tabBank Account` ba ON pe.paid_to = ba.account
-	WHERE
-		payment_type = 'Pay'
-		AND pe.docstatus = 1
-        AND pe.posting_date >= '{filters.get("from_date")}'
-        AND pe.posting_date <= '{filters.get("to_date")}'
-	GROUP BY
-		pe.party,
-        pe.party_name
+    # supplier_sql = f"""
+    # SELECT
+	# 	pe.party AS supplier,
+    #     pe.party_name,
+    #     {",".join(_banks)}
+	# FROM
+	# 	`tabPayment Entry` pe
+    # LEFT JOIN
+	# 	`tabBank Account` ba ON pe.paid_to = ba.account
+	# WHERE
+	# 	payment_type = 'Pay'
+	# 	AND pe.docstatus = 1
+    #     AND pe.posting_date >= '{filters.get("from_date")}'
+    #     AND pe.posting_date <= '{filters.get("to_date")}'
+	# GROUP BY
+	# 	pe.party,
+    #     pe.party_name
     
-    """
-    total_supplier_sql = f"""
-    SELECT
-        'Total' as supplier,
-        '' as party_name,
-        {",".join(_banks)}
-    FROM
-        `tabPayment Entry` pe
-    LEFT JOIN
-        `tabBank Account` ba ON pe.paid_to = ba.account
-    WHERE
-        pe.payment_type = 'Pay'
-        AND pe.docstatus = 1
-        AND pe.posting_date >= '{filters.get("from_date")}'
-        AND pe.posting_date <= '{filters.get("to_date")}'
+    # """
+    # total_supplier_sql = f"""
+    # SELECT
+    #     'Total' as supplier,
+    #     '' as party_name,
+    #     {",".join(_banks)}
+    # FROM
+    #     `tabPayment Entry` pe
+    # LEFT JOIN
+    #     `tabBank Account` ba ON pe.paid_to = ba.account
+    # WHERE
+    #     pe.payment_type = 'Pay'
+    #     AND pe.docstatus = 1
+    #     AND pe.posting_date >= '{filters.get("from_date")}'
+    #     AND pe.posting_date <= '{filters.get("to_date")}'
 
-    """
+    # """
     
     customer_data = frappe.db.sql(customer_sql)
     total_customer = frappe.db.sql(total_customer_sql)
-    supplier_data = frappe.db.sql(supplier_sql)
-    total_supplier = frappe.db.sql(total_supplier_sql)
 
-    data = customer_data + total_customer + supplier_data + total_supplier
+    # supplier_data = frappe.db.sql(supplier_sql)
+    # total_supplier = frappe.db.sql(total_supplier_sql)
+
+    data = customer_data + total_customer
     return columns, data
 
 
