35e9e6b6fae9111ac32b5784b75209a7170b4df8
RubNawaz1 <rubnawaz.mahar@gmail.com>
2025-06-04 12:51:41 +0500

    Update Account Dashboard and Number Cards

--- ttpl/ttpl/number_card/cash_balances/cash_balances.py
+++ ttpl/ttpl/number_card/cash_balances/cash_balances.py
@@ -2,22 +2,27 @@ import frappe
 
 @frappe.whitelist()
 def get_cash_balances():
-    query = """
-        SELECT
-            SUM(debit) AS total_debit,
-            SUM(credit) AS total_credit,
-            SUM(debit - credit) AS balance
-        FROM
-            `tabGL Entry`
-        WHERE
-            `account` IN (
-                '10202001 - CASH IN HAND HEAD OFFICE - TTPL',
-                '10202002 - CASH IN HAND FACTORY - TTPL'
-            )
-            AND is_cancelled = 0
-    """
-    value = frappe.db.sql(query)[0][0] or 0.0
+    accounts = [
+        '10202001 - CASH IN HAND HEAD OFFICE - TTPL',
+        '10202002 - CASH IN HAND FACTORY - TTPL'
+    ]
+    
+    company = "Tapal Tex Pvt. Ltd"
+    balance = 0.0
+
+    for account in accounts:
+        if frappe.db.exists("Account", account):
+            # Get balance from GL Entry
+            result = frappe.db.sql("""
+                SELECT SUM(debit) - SUM(credit) AS balance
+                FROM `tabGL Entry`
+                WHERE account = %s AND company = %s
+            """, (account, company), as_dict=True)
+
+            account_balance = result[0].balance or 0.0
+            balance += account_balance
+
     return {
-        "value": value,
+        "value": balance,
         "fieldtype": "Currency"
     }
