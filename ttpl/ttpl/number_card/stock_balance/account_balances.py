d28a5ff2cabfb63f20ad012c44bfefe4c65812b0
RubNawaz1 <rubnawaz.mahar@gmail.com>
2025-06-04 10:50:44 +0500

    Update Account Dashboard and Number Cards

--- ttpl/ttpl/number_card/stock_balance/account_balances.py
+++ ttpl/ttpl/number_card/stock_balance/account_balances.py
@@ -17,11 +17,18 @@ def get_account_balance():
                 '10205001 - STOCK IN TRADE - TTPL',
                 '10205002 - STOCK IN TRANSIT - TTPL',
                 '10205004 - HEATER FUEL MATERIAL - TTPL',
-                '10206000 - RAW MATERIAL STORAGE - TTPL',
+                '10206001 - Polyester Virging - TTPL',
+                '10206002 - Viscose Virgin - TTPL',
+                '10206003 - Polyester Pet Re-Cycle - TTPL',
+                '10206004 - Cotton - TTPL',
+                '10206005 - PP Cardfly - TTPL',
+                '10206006 - Mix Material - TTPL',
+                '10206007 - RAW MATERIAL (ALL TYPES) - TTPL',
                 '10207003 - FINISHED GOODS (ALL GSM STOCK IN HAND) - TTPL',
                 '10209000 - WORK IN PROCESS - TTPL'
             )
             AND is_cancelled = 0
+
     """
 
     value = frappe.db.sql(query)[0][0] or 0.0
